68e6152d58d9995d7c36ff82db3c594502991d27
Tom Wittmann <WittCode@gmail.com>
2020-10-13 21:42:55 -0400

    Managed to return the captured packet as a string with all whitespace stripped. This was done through show(dump=True).

--- my_sniffer.py
+++ my_sniffer.py
@@ -1,9 +1,21 @@
-
+import scapy.all as scapy
+import io
+import sys
 '''
 @:param event - The clicking of the button.
 '''
-def start_sniff(event):
-    print('Sniffing')
+def start_sniff(counts):
+    scapy.sniff(prn=packet_callback, count=counts)
+
 
 def stop_sniff():
     print('Stopping')
+
+
+# Parse the response.
+def packet_callback(packet):
+    capture = packet.show(dump=True)
+    to_parse = capture.replace(' ', '')
+    parse_two = to_parse.replace('\n', '')
+    print('SOCCER:\n' + parse_two)
+
