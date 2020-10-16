8d01e4821a9a24dd82a8453ad5a546dc6a33f75f
Tom Wittmann <WittCode@gmail.com>
2020-10-15 22:07:33 -0400

    Works only with IPv4. IPv6 and ARP mess up the application.

--- my_sniffer.py
+++ my_sniffer.py
@@ -1,10 +1,14 @@
 import scapy.all as scapy
 import re
 
-'''
-@:param event - The clicking of the button.
-'''
+
 def start_sniff(counts):
+    """
+    start_sniff captures packets and calls a callback.
+    :param p - Packet to capture.
+    :param counts - How many packets to capture.
+    :param dictionary = Dictionary of source and destination IPs.
+    """
     scapy.sniff(prn=packet_callback, count=counts)
 
 
@@ -12,9 +16,24 @@ def stop_sniff():
     print('Stopping')
 
 
+def get_dict(l, capture):
+    """
+    :param l:       List to hold IP addresses.
+    :param capture: The string containing the packet.
+    :return:        Dictionary of source IP addresses.
+    """
+
+
+
 # Parse the response.
 def packet_callback(packet):
+
     # dump=True returns the packet as a string, get rid of all whitespace.
     capture = re.sub(r'[\n\t\s]', '', packet.show(dump=True))
     print(capture)
+    # Find all the ip addresses regex.
+    # Issue is need IPv4 addresses only. ARP and IPv6 mess it up.
+    ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', capture)
+    print(ip)
+
 
--- tomcat_gui.py
+++ tomcat_gui.py
@@ -8,11 +8,16 @@ root = tk.Tk()
 # Title of the window.
 root.title('Tomcat')
 
+# Frame to hold both the buttons.
 frame_bottom = tk.Frame(master=root)
 
+counts = 5
+
 # When a function is called without parentheses a function reference is sent to the callable.
-button_sniff = tk.Button(master=frame_bottom, text='Sniff', width=15)
-button_sniff.bind(sequence='<Button-1>', func=start_sniff)
+# When you use the lambda command it returns a reference to the created function.
+button_sniff = tk.Button(master=frame_bottom, text='Sniff', width=15,
+                         command=lambda: start_sniff(counts=counts))
+
 button_stop = tk.Button(master=frame_bottom, text='Stop', command=stop_sniff, width=15)
 button_sniff.pack(padx=10, pady=5, side=tk.LEFT)
 button_stop.pack(padx=10, pady=5)
