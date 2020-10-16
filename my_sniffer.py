6e88f344926f0c5d37c885b5981e21d8f8455331
Tom Wittmann <WittCode@gmail.com>
2020-10-15 23:34:36 -0400

    Click the sniff button and it prints to the console the first and second IP address if the packet contains 'IPv4'. Skeleton code of stop sniffing.

--- my_sniffer.py
+++ my_sniffer.py
@@ -2,18 +2,23 @@ import scapy.all as scapy
 import re
 
 
-def start_sniff(counts):
+def start_sniff():
     """
     start_sniff captures packets and calls a callback.
     :param p - Packet to capture.
     :param counts - How many packets to capture.
     :param dictionary = Dictionary of source and destination IPs.
     """
-    scapy.sniff(prn=packet_callback, count=counts)
+    scapy.sniff(prn=packet_callback, stop_filter=stop_sniff)
 
 
-def stop_sniff():
-    print('Stopping')
+def stop_sniff(packet):
+    """
+    # Function to check whether sniffing should stop.
+    :param packet: The packet we can use to check if we should stop.
+    :return: True to stop sniffing.
+    """
+    return True
 
 
 def get_dict(l, capture):
@@ -24,16 +29,20 @@ def get_dict(l, capture):
     """
 
 
-
-# Parse the response.
 def packet_callback(packet):
-
+    """
+    Called when a packet is sniffed. Parse the packet and return IP addresses.
+    :param packet: Packet that was sniffed.
+    :return: List of IP addresses.
+    """
     # dump=True returns the packet as a string, get rid of all whitespace.
     capture = re.sub(r'[\n\t\s]', '', packet.show(dump=True))
-    print(capture)
-    # Find all the ip addresses regex.
-    # Issue is need IPv4 addresses only. ARP and IPv6 mess it up.
-    ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', capture)
-    print(ip)
+    if ('type=IPv4' in capture):
+        print("IPv4")
+        # Find all the ip addresses regex.
+        # Issue is need IPv4 addresses only. ARP and IPv6 mess it up.
+        ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', capture)
+        print('src={}'.format(ip[0]))
+        print('dst={}'.format(ip[1]))
 
 
--- tomcat_gui.py
+++ tomcat_gui.py
@@ -11,12 +11,10 @@ root.title('Tomcat')
 # Frame to hold both the buttons.
 frame_bottom = tk.Frame(master=root)
 
-counts = 5
-
 # When a function is called without parentheses a function reference is sent to the callable.
 # When you use the lambda command it returns a reference to the created function.
 button_sniff = tk.Button(master=frame_bottom, text='Sniff', width=15,
-                         command=lambda: start_sniff(counts=counts))
+                         command=start_sniff)
 
 button_stop = tk.Button(master=frame_bottom, text='Stop', command=stop_sniff, width=15)
 button_sniff.pack(padx=10, pady=5, side=tk.LEFT)
