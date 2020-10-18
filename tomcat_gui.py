3dd33ce8ddb5df9b6d9db7f8cdc1c46120a6b24a
Tom Wittmann <WittCode@gmail.com>
2020-10-18 17:27:49 -0400

    Deleted unnecessary code. Was finally able to pass multiple parameters to the scappy sniff function prn argument function. Currently finds IP addresses in IPv4 packets and makes labels containing the source and destination IP.

--- tomcat_gui.py
+++ tomcat_gui.py
@@ -1,15 +1,15 @@
 import tkinter as tk
-from tkinter import ttk
-from my_sniffer import *
-from my_packet import *
+import scapy.all as scapy
+from packet_process import *
 
 # Main window.
 root = tk.Tk()
 root.title('Tomcat')
 root.geometry('500x500')
 
-button_sniff = tk.Button(master=root, text='Sniff', width=15)
-
+button_sniff = tk.Button(master=root, text='Sniff', width=15,
+                         command=lambda: scapy.sniff(prn=custom_action(root),
+                                                     count=1))
 button_sniff.pack()
 
 # Display the GUI.
